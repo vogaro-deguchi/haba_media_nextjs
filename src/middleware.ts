77fccbf617de1332033a1841e188fa5d88d8d98e
vogaro-deguchi <h.deguchi@vogaro.co.jp>
2023-08-18 16:57:41 +0900

    [fixed] middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -20,7 +20,7 @@ export function middleware(req: NextRequest) {
         return NextResponse.next();
       }
     }
-    url.pathname = "/api/auth";
+    url.pathname = "/app/api/auth";
     return NextResponse.rewrite(url);
   }
   return NextResponse.next();
