8ec5f16726e608771c2b548e60cd17774382d030
vogaro-deguchi <h.deguchi@vogaro.co.jp>
2023-08-18 16:04:04 +0900

    [update] json管理更新

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import { log } from 'console';
 import styles from './style.module.scss'
 
 import Article from '@/components/Article/article'
 
-type TodoItem = {
+import articleData from '../../article.json'
+
+type articleType = {
   userId: number;
   id: number;
   category: string;
@@ -13,32 +14,13 @@ type TodoItem = {
 }
 
 export default function Home() {
-  const todos: TodoItem[] = [
-    {
-      "userId": 1,
-      "id": 1,
-      "category": "商品コラム",
-      "title": "この文章はダミーです。文字の大きさ、量、時間、行間を確認するために入れています。",
-      "text": "この文章はダミーです文字の大きさ、量、時間、行間を確認するために入れています。この文章はダミーです文字の大きさ、量、時間、行間を確認するために入れています。",
-      "tags": ["スキンケア", "しわ", "くすみ"]
-    },
-    {
-      "userId": 2,
-      "id": 2,
-      "category": "ニュース",
-      "title": "この文章はダミーです。文字の大きさ、量、時間、行間を確認するために入れています。",
-      "text": "この文章はダミーです文字の大きさ、量、時間、行間を確認するために入れています。この文章はダミーです文字の大きさ、量、時間、行間を確認するために入れています。",
-      "tags": ["スキンケア", "しわ"]
-    }
-  ];
   return (
     <section className={styles.secArticle}>
-      {todos.map((todo: TodoItem) => {
-        console.log(todo);
+      {articleData.map((data: articleType) => {
         return (
           <Article 
-            key = {todo.id}
-            {...todo}
+            key = {data.id}
+            {...data}
           />
         )
       })}
