41b9c12ba1b877de01a035dbc1bc45b7db9dbc8b
vogaro-deguchi <h.deguchi@vogaro.co.jp>
2023-08-18 16:30:14 +0900

    [update] Basic認証追加

---
+++ src/app/middleware.ts
@@ -0,0 +1,27 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+export const config = {
+  matcher: "/:path*",
+};
+
+export function middleware(req: NextRequest) {
+  if (process.env.NEXT_PUBLIC_VERCEL_ENV === "preview") {
+    const basicAuth = req.headers.get("authorization");
+    const url = req.nextUrl;
+    if (basicAuth) {
+      const authValue = basicAuth.split(" ")[1];
+      const [user, pwd] = atob(authValue).split(":");
+
+      if (
+        user === process.env.BASIC_USERNAME &&
+        pwd === process.env.BASIC_PASSWORD
+      ) {
+        return NextResponse.next();
+      }
+    }
+    url.pathname = "/api/auth";
+    return NextResponse.rewrite(url);
+  }
+  return NextResponse.next();
+}
