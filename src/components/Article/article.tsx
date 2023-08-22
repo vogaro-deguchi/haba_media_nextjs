393a8243bbd53ddf3b1f377884a17db944b1ef8c
vogaro-deguchi <h.deguchi@vogaro.co.jp>
2023-08-22 16:47:07 +0900

    [fixed] コンテンツモデル修正

--- src/components/Article/article.tsx
+++ src/components/Article/article.tsx
@@ -7,29 +7,28 @@ import Link from 'next/link'
 export default function Article(props: any): JSX.Element {
   return (
     <article className={styles.article}>
-      <Link href="" className={styles.articleLink}>
-        <div className={styles.articleImg}>
-          <Image
-            src='/assets/images/index/article_01.png'
-            alt=''
-            width={360}
-            height={360}
-            objectFit="cover"
-          />
+      <Link href="" className={styles.articleLink} />
+      <div className={styles.articleImg}>
+        <Image
+          src='/assets/images/index/article_01.png'
+          alt=''
+          width={360}
+          height={360}
+          objectFit="cover"
+        />
+      </div>
+      <div className={styles.articleContent}>
+        <span className={styles.articleCategory}>{props.category}</span>
+        <h2 className={styles.articleTitle}>{props.title}</h2>
+        <p className={styles.articleText}>{props.text}</p>
+        <div className={styles.articleTagList}>
+          {props.tags.map((tag: any) => {
+            return (
+              <Link className={styles.articleTag} href="" key={tag.id}>{tag}</Link>
+            )
+          })}
         </div>
-        <div className={styles.articleContent}>
-          <span className={styles.articleCategory}>{props.category}</span>
-          <h2 className={styles.articleTitle}>{props.title}</h2>
-          <p className={styles.articleText}>{props.text}</p>
-          <div className={styles.articleTagList}>
-            {props.tags.map((tag: any) => {
-              return (
-                <Link className={styles.articleTag} href="" key={tag.id}>{tag}</Link>
-              )
-            })}
-          </div>
-        </div>
-      </Link>
+      </div>
     </article>
   )
 }
