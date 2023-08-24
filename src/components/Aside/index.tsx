34dcc9c32dadc7001acabf35bc18662d57e88ea9
vogaro-deguchi <h.deguchi@vogaro.co.jp>
2023-08-24 12:34:42 +0900

    [update] Aside sticky

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -18,38 +18,40 @@ type articleType = {
 export default function Footer() {
   return (
     <aside className={styles.aside}>
-      <div className={styles.category}>
-        <Image 
-          src="/assets/images/icon_category.svg"
-          alt=""
-          width ={30}
-          height ={30}
-        />
-        <p className={styles.asideTitle}>記事カテゴリ</p>
-        <ul className={styles.categoryList}>
-          <li><a href="">商品コラム</a></li>
-          <li><a href="">スタッフコラム</a></li>
-          <li><a href="">美容情報</a></li>
-          <li><a href="">インナーケア</a></li>
-          <li><a href="">ライフスタイル</a></li>
-        </ul>
-      </div>
-      <div className={styles.ranking}>
-        <Image 
-          src="/assets/images/icon_ranking.svg"
-          alt=""
-          width ={30}
-          height ={30}
-        />
-        <p className={styles.asideTitle}>人気記事ランキング</p>
-        {articleData.map((data: articleType) => {
-          return (
-            <RankingArticle
-              key = {data.id}
-              {...data}
-            />
-          )
-        })}
+      <div  className={styles.asideInner}>
+        <div className={styles.category}>
+          <Image 
+            src="/assets/images/icon_category.svg"
+            alt=""
+            width ={30}
+            height ={30}
+          />
+          <p className={styles.asideTitle}>記事カテゴリ</p>
+          <ul className={styles.categoryList}>
+            <li><a href="">商品コラム</a></li>
+            <li><a href="">スタッフコラム</a></li>
+            <li><a href="">美容情報</a></li>
+            <li><a href="">インナーケア</a></li>
+            <li><a href="">ライフスタイル</a></li>
+          </ul>
+        </div>
+        <div className={styles.ranking}>
+          <Image 
+            src="/assets/images/icon_ranking.svg"
+            alt=""
+            width ={30}
+            height ={30}
+          />
+          <p className={styles.asideTitle}>人気記事ランキング</p>
+          {articleData.map((data: articleType) => {
+            return (
+              <RankingArticle
+                key = {data.id}
+                {...data}
+              />
+            )
+          })}
+        </div>
       </div>
     </aside>
   )
